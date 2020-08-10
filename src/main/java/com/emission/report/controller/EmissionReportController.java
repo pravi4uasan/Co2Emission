2a2c525f2677142377e3d8ada729a3a9c44104c8
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-08-09 22:36:29 -0400

    Update EmissionReportController.java

--- src/main/java/com/emission/report/controller/EmissionReportController.java
+++ src/main/java/com/emission/report/controller/EmissionReportController.java
@@ -1,8 +1,24 @@
 package com.emission.report.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.emission.report.entity.CustomerDetails;
+import com.emission.report.service.EmissionReportService;
 
 @RestController
 public class EmissionReportController {
 
+	@Autowired
+	private EmissionReportService service;
+	
+	@PostMapping("/sensorData")
+	public void addEmissionData(CustomerDetails req) {
+		
+		service.addEmissionData(req);
+		
+	}
+	
+	
 }
