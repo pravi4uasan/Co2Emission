b8d63e26f6da716908d1956577c9736f57e7178c
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-08-11 05:02:00 +0530

    changes for H2 testing
    changes for H2 testing

--- src/main/java/com/emission/report/entity/SensorReading.java
+++ src/main/java/com/emission/report/entity/SensorReading.java
@@ -2,11 +2,14 @@ package com.emission.report.entity;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -35,5 +38,10 @@ public class SensorReading {
 
 	@Column(name="SENSORREADING_DATETIME")
 	private Date sensorDateTime;
+	
+	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+			 CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="SENSORDETAILS_ID")
+    private SensorDetail sensorDetail;
 
 }
