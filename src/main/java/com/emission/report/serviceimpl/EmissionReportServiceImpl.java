a4a7264d17f67e63b0f3bee24cc2850013499daa
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-08-09 22:38:52 -0400

    Update EmissionReportServiceImpl.java

--- src/main/java/com/emission/report/serviceimpl/EmissionReportServiceImpl.java
+++ src/main/java/com/emission/report/serviceimpl/EmissionReportServiceImpl.java
@@ -1,8 +1,23 @@
 package com.emission.report.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emission.report.entity.CustomerDetails;
+import com.emission.report.repositories.EmissionReportRepository;
+import com.emission.report.service.EmissionReportService;
+
 @Service
-public class EmissionReportServiceImpl {
+public class EmissionReportServiceImpl implements EmissionReportService{
+
+	@Autowired
+	private EmissionReportRepository repo;
+	
+	@Override
+	public void addEmissionData(CustomerDetails req) {
+		
+		repo.save(req);
+		
+	}
 
 }
